43edadc20e8dad74252c07fa91b2f7c7fc2912ed
Oscar Alberto <oscar@gmail.co>
2023-05-09 22:45:39 -0700

    add text and delete btn

--- popup/popup.js
+++ popup/popup.js
@@ -2,17 +2,29 @@ const addTaskBtn = document.getElementById("add-task-btn");
 
 addTaskBtn.addEventListener("click", ()=> addTask())
 
+// save the task list
+const tasks =  []
+
 function addTask() {
+  const item = tasks.length
+  tasks.push("")
+
   const taskRow = document.createElement("div")
 
   const text = document.createElement("input")
   text.type= "text";
   text.placeholder="Enter a task...";
+  text.addEventListener("change" , ()=> {
+    tasks[item] = text.value
+    console.log(tasks)
+  })
 
   const deleteBtn = document.createElement("input")
   deleteBtn.type="button";
   deleteBtn.value = "X";
-
+  deleteBtn.addEventListener('click' , ()=>{
+     tasks.splice(item,1)
+  })
   taskRow.appendChild(text)
   taskRow.appendChild(deleteBtn)
 
