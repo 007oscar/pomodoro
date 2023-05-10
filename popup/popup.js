bd8eae18ef0e2c47aa992e6ece14659577892503
Oscar Albert <oscar@google.co>
2023-05-10 16:47:33 -0700

    add function to delete button

--- popup/popup.js
+++ popup/popup.js
@@ -1,22 +1,51 @@
 const addTaskBtn = document.getElementById("add-task-btn");
 
-addTaskBtn.addEventListener("click", ()=> addTask())
+addTaskBtn.addEventListener("click", () => addTask());
+const tasks = [];
 
-function addTask() {
-  const taskRow = document.createElement("div")
+function renderTask(item) {
+  // const item = tasks.length;
+  // tasks.push("");
 
-  const text = document.createElement("input")
-  text.type= "text";
-  text.placeholder="Enter a task...";
+  const taskRow = document.createElement("div");
 
-  const deleteBtn = document.createElement("input")
-  deleteBtn.type="button";
+  const text = document.createElement("input");
+  text.type = "text";
+  text.placeholder = "Enter a task...";
+  text.value = tasks[item];
+  text.addEventListener("change", () => {
+    tasks[item] = text.value;
+    console.log(tasks);
+  });
+  const deleteBtn = document.createElement("input");
+  deleteBtn.type = "button";
   deleteBtn.value = "X";
 
-  taskRow.appendChild(text)
-  taskRow.appendChild(deleteBtn)
+  taskRow.appendChild(text);
+  taskRow.appendChild(deleteBtn);
 
-  const taskContainer = document.getElementById("task-container")
-  taskContainer.appendChild(taskRow)
-  
+  deleteBtn.addEventListener("click", () => {
+    // tasks.splice(item, 1);
+    deleteTask(item);
+  });
+  const taskContainer = document.getElementById("task-container");
+  taskContainer.appendChild(taskRow);
+}
+
+function renderTasks() {
+  const taskContainer = document.getElementById("task-container");
+  taskContainer.textContent = ""
+  tasks.forEach((value, index) => {
+    console.log(value, index);
+    renderTask(index);
+  });
+}
+function addTask() {
+  tasks.push("");
+  renderTasks();
+}
+
+function deleteTask(item) {
+  tasks.splice(item, 1);
+  renderTasks();
 }
